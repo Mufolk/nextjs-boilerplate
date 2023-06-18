60d0c943982e607f7a4b09a3af1b25a9c7a0d564
mufolk <guilhermemelo138@gmail.com>
2023-06-18 19:30:17 -0300

    setting up husky and scripts for the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'next/core-web-vitals',
-    'prettier',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'standard-with-typescript',
+        'plugin:react/recommended',
+        'next/core-web-vitals',
+        'prettier',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: 'tsconfig.eslint.json',
+        tsconfigRootDir: './',
+    },
+    plugins: ['react'],
+    rules: {},
 }
